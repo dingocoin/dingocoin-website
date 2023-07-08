12dca8b667dd0269705982ac354b3515b9321ace
BobTheBuilderSTD <chris@supertechdudes.com>
2023-07-08 08:45:04 -0500

    Minor Link updates
    Updated url links

--- Branding.php
+++ Branding.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-    <title>Dingocoin | Home</title>
+    <title>Dingocoin | Wallets</title>
 
     <meta content="Dingocoin is the most successful cryptocurrency fork of Dogecoin, with revolutionary innovations and unstoppable developments." name="description">
     <meta content="SuperTechDudes in conjunction with Dingocoin Project" name="author">
@@ -65,67 +65,44 @@
     gtag('config', 'G-P30Q0K21WP');
 </script>
 <body>
-    <?php include ('includes/pages/top_nav.html');?>
-
-<header>
-    <a href="/Wallets">    
-        <div id="carousel" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="d-block w-100" src="/data1/images/121a.gif" alt="Get Involved Header">
-                </div>
+    <?php include('includes/pages/top_nav.html'); ?>
+    <div id="carousel" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="/data1/images/hwallet.jpg"
+                    alt="Wallet Header">
             </div>
-        </div></a>
-</header>
-<?php
-$domain = 'https://explorer.dingocoin.com';  // Replace with the domain you want to check
-$headers = @get_headers($domain);
-if (!$headers || strpos($headers[0], '200') === false) {
-    // Domain is down or not returning a 200 OK status code
-    // Exclude the file here or perform any other necessary actions
-    } else {
-    // Domain is up and running
-    include ('includes/pages/net_data.php');
-    }
-if (!@include('includes/pages/about.html')) {
-    echo '<div class="container-xxl py-5">
-	        <div class="container">
-                <div class="text-center mx-auto  fadeInUp" data--delay="0.1s" style="max-width: 500px;">
-                    <h1 class="display-6">About Us</h1>
-                    </div>
+        </div>
+    </div>
+    <?php
+    if (!@include('includes/pages/wallets.html')) {
+        echo '<div class="container-xxl py-5">
+	                <div class="container">
+                        <div class="text-center mx-auto  fadeInUp" data--delay="0.1s" style="max-width: 500px;">
+                            <h1 class="display-6">Dingocoin Wallets</h1>
+                        </div>
                         <div class="row justify-content-center text-center">
-                            <span>About Us Module Loading ...... <br> <small>Error: std-dxQH1</small></span>
+                            <span>Wallets Module Loading ......<br> <small>Error: <small>Error: std-dxQH1</small></span>
                         </div> 
                     </div>
-                </div>';
-            }
-    if (!@include('includes/pages/featured_projects.html')) {
-        echo '<div class="container-xxl py-5">
-	        <div class="container">
-                <div class="text-center mx-auto  fadeInUp" data--delay="0.1s" style="max-width: 500px;">
-                        <h1 class="display-6">Featured Projects</h1>
-                    </div>
-                    <div class="row justify-content-center text-center">
-                        <span>Featured Projects Module Loading ......<br> <small>Error: std-dxQH1</small></span>
-                    </div> 
-                </div>
-            </div>
-            ';
-        }
-    include ('includes/pages/footer.html');
-    include ('includes/pages/2top.html'); ?>
+                  </div>
+                 ';
+    }
+    ?>
+
+    <?php include('includes/pages/footer.html'); ?>
+    <?php include('includes/pages/2top.html'); ?>
 
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="/lib/wow/wow.min.js"></script>
-    <script src="/lib/easing/easing.min.js"></script>
-    <script src="/lib/waypoints/waypoints.min.js"></script>
-    <script src="/lib/owlcarousel/owl.carousel.min.js"></script>
-    <script src="/lib/counterup/counterup.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="/engine1/slider.js"></script>
-	<script type="text/javascript" src="engine1/script.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.js"></script>
 
 </body>
 </html>
