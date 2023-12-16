333ace5ac33b0f619aa3e132e188dc854f901931
BobTheBuilderSTD <108704846+BobTheBuilderSTD@users.noreply.github.com>
2023-12-16 14:53:44 -0600

    Dynamic Alert
    Improved Dynamic Alert Message System Site.
    - Expanded From Trailmap to sitewide status
    - New Messages display based on status
    
     	trailmap_soon: It calculates the countdown to a target date and time ("year-month-day hr:min:sec") and displays the remaining days, hours, and minutes until that date.
    
    	trailmap_now: Displays the message "New Trailmap Update Live Now."
    
    	new_section:  Displays a message indicating that a new section is now open and prompts the user to check out the specified section name.
    
    	custom_alert: Provides a placeholder message, indicating that you can enter a custom message here.
    
    	Default: Default message welcoming the user to Dingocoin.
    
    	set via value of $selectedMessage

--- includes/pages/trailmap-alert.php
+++ includes/pages/trailmap-alert.php
@@ -1,26 +1,46 @@
 <div class="alert alert-info text-center" role="alert">
     <?php
-    // Countdown Clock - Uncomment To Activate
-    // Set the target date and time
-    //$target_date = "2023-03-15 23:59:59";
+    // Set the message variable
+    $selectedMessage = '';
 
-    // Get the current date and time
-    //$current_date = date('Y-m-d H:i:s');
+    // Display different messages based on the selected value
+    switch ($selectedMessage) {
+        case 'trailmap_soon':
+            // Countdown Clock
+            // Set the target date and time
+            $target_date = "2023-03-15 23:59:59";
 
-    // Calculate the difference between the target date and the current date
-    //$diff = strtotime($target_date) - strtotime($current_date);
+            // Get the current date and time
+            $current_date = date('Y-m-d H:i:s');
+            
+            // Calculate the difference between the target date and the current date
+            $diff = strtotime($target_date) - strtotime($current_date);
+            
+            // Calculate the remaining days, hours, minutes, and seconds
+            $days = floor($diff / (60 * 60 * 24));
+            $hours = floor(($diff % (60 * 60 * 24)) / (60 * 60));
+            $minutes = floor(($diff % (60 * 60)) / 60);
+            
+            // Display the countdown clock
+            
+            // Countdown Alert Message
+            echo "New Trailmap Update Coming Soon! Time remaining: " . $days . " days, " . $hours . " hours, " . $minutes . " minutes, ";                          
+            break;    
 
-    // Calculate the remaining days, hours, minutes, and seconds
-    //$days = floor($diff / (60 * 60 * 24));
-    //$hours = floor(($diff % (60 * 60 * 24)) / (60 * 60));
-    //$minutes = floor(($diff % (60 * 60)) / 60);
+        case 'trailmap_now':
+            echo "New Trailmap Update Live Now";
+            break;
 
-    // Display the countdown clock
+        case 'new_section':
+            echo "New Section Now Open! See New Our New (Section Name) Section";
+            break;
+            
+        case 'custom_alert':
+            echo "Enter Custom Message Here";
+            break;        
 
-    // Countdown Alert - uncomment if using countdown
-    //echo "New Trailmap Update Coming Soon! Time remaining: " . $days . " days, " . $hours . " hours, " . $minutes . " minutes, ";
-    
-    // New Trailmap Available Alert
-    echo "New Feature! Check Out Dingocoin Payments Now Available!";
+        default:
+            echo "Welcome To Dingocoin. Much King Dingo Such Wild Doge";
+    }
     ?>
 </div>
